bbfa77a55f2d97729d6cf0e673f7c55ce6be5c6c
snehasamuel <snehasamuel297@gmail.com>
2022-05-17 21:28:31 +0530

    search  pro completed

--- src/components/Proadd.js
+++ src/components/Proadd.js
@@ -9,7 +9,7 @@ const Proadd = () => {
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-<div className="row">
+<div className="row g-3">
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 <label for="" className="form-label">Product Name</label>
 <input type="text" className="form-control"/>
