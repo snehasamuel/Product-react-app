2ecac92fd3b4b40ceef97d451f570f2937354ea8
snehasamuel <snehasamuel297@gmail.com>
2022-05-18 16:02:06 +0530

    fetching

--- src/components/Searchpro.js
+++ src/components/Searchpro.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Searchpro = () => {
-    
+    var [proname,setProname]=useState("")
+
+const changeValue=()=>{
+    const data=
+}
+
   return (
     <div>
         <Header/>
@@ -14,10 +19,10 @@ const Searchpro = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Product Name</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(s)=>{setProname(s.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-success">SEARCH</button>
+                    <button onClick={} className="btn btn-success">SEARCH</button>
                 </div>
             </div>
 
