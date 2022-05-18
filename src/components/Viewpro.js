095b6c7a68de89c2f6350b99d66d82182a41f86f
snehasamuel <snehasamuel297@gmail.com>
2022-05-18 09:46:10 +0530

    View branch pending

--- src/components/Viewpro.js
+++ src/components/Viewpro.js
@@ -7,14 +7,13 @@ const Viewpro = () => {
     <div>
         <Header/>
         
-      {prolist.map((value,key) => {
-
-return <div className="container">
+      <div className="container">
 <div className="row">
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
         <div className="row g-3">
-            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+        {prolist.map((value,key) => {
+        return <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
             <div className="card" >
   <img src={value.image} className="card-img-top" alt="..."/>
   <div className="card-body">
@@ -29,14 +28,14 @@ return <div className="container">
   </div>
 </div>
            </div>
-        </div>
-
-    </div>
-</div>
-</div>
+        
 
        })}
+</div>
 
+</div>
+</div>
+</div>
     </div>
   )
 }
